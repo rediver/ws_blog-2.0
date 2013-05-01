29b32bef65374e6bd328b42bc04c1d5d12bb6ed3
wojtek <wojciech.smajda@gmail.com>
2013-05-01 00:44:50 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WsCms::Application.config.secret_token = '53b891f7a4e56920e2e4f0c7e5ce1f59642e4b09f58ac792f7283c9df5f0ca6eef42202d0ccf70b235454652cedb26b1faac7f9a348550e04b4b2a6b6fc91d42'
