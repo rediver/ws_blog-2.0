bf95aba701c8e9bfd84cec233e57afce69815835
wojtek <wojciech.smajda@gmail.com>
2013-08-09 15:45:28 +0000

    contact_us gem config

--- config/initializers/contact_us.rb
+++ config/initializers/contact_us.rb
@@ -6,18 +6,18 @@ ContactUs.setup do |config|
   # Configure the e-mail address which email notifications should be sent from.  If emails must be sent from a verified email address you may set it here.
   # Example:
   # config.mailer_from = "contact@please-change-me.com"
-  config.mailer_from = nil
+  config.mailer_from = "wojciech.smajda@gmail.com"
 
   # Configure the e-mail address which should receive the contact form email notifications.
-  config.mailer_to = "contact@please-change-me.com"
+  config.mailer_to = "wojciech.smajda@gmail.com"
 
   # ==> Form Configuration
 
   # Configure the form to ask for the users name.
-  config.require_name = false
+  config.require_name = true
 
   # Configure the form to ask for a subject.
-  config.require_subject = false
+  config.require_subject = true
 
   # Configure the form gem to use.
   # Example:
