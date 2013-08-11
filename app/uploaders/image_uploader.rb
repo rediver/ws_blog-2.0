a7459ed6d60bd5453861ae09acb64656737d0ce0
wojtek <wojciech.smajda@gmail.com>
2013-08-11 14:18:37 +0000

    fixes proper thumb size

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -40,7 +40,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_limit => [500, 500]
+    process :resize_to_limit => [860, 860]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
